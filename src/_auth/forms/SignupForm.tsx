f5329e6654cd6fc4a938d596bd8fa4da9311f45e
Arshid Bhat <arshid.bhat@hexaviewtech.com>
2024-03-17 01:57:48 +0530

    pixelgram app basic setup and routes setup

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,14 @@
+
+const SignupForm = () => {
+  return (
+    <>
+        <div className="flex flex-col justify-center items-center">
+            <img src='/assets/images/logo.svg' alt='logo' className="pt-10" />
+            <h2 className="text-2xl font-semibold font-sans mt-5">Create a new account</h2>
+            <p className="text-base text-slate-600 font-sans mt-1">To use use PixelGram, please enter your details</p>
+        </div>
+    </>
+  )
+}
+
+export default SignupForm
