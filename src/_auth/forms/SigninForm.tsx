73fe666dad49c6c86e09b284c08f8386cd2aba2c
Arshid Bhat <arshid.bhat@hexaviewtech.com>
2024-03-17 23:27:25 +0530

    signup and login page design

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,8 +1,75 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SigninForm = () => {
-  return (
-    <div>SigninForm</div>
-  )
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onSubmit = () => {
+
+    }
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value  
+        }));
+    }
+
+    return (
+        <>
+            <div className="flex flex-col items-center w-full">
+                <section className="flex flex-col justify-center items-center">
+                    <img src='/assets/images/logo.svg' alt='logo' className="pt-10" />
+                    <h2 className="text-2xl xl:text-3xl font-semibold font-sans mt-5">Create a new account</h2>
+                    <p className="text-[12px] xl:text-[16px] text-slate-600 font-sans mt-1">To use use PixelGram, please enter your details</p>
+                </section>
+
+                <section className="m-4 w-2/3">
+                    <form onSubmit={onSubmit} className="flex flex-col gap-5">
+                        <div className="">
+                            <p className="font-serif mb-2 text-[16px]">Email</p>
+                            <input 
+                                type="email"
+                                className="pl-1 w-full h-12 bg-[#1F1F22] border-none placeholder:text-light-4 focus-visible:ring-1 focus-visible:ring-offset-1 ring-offset-light-3 !important"
+                                id="email"
+                                name="email"
+                                value={email}
+                                placeholder="Enter your email"
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="">
+                            <p className="font-serif mb-2 text-[16px]">Password</p>
+                            <input 
+                                type="password"
+                                className="pl-1 w-full h-12 bg-[#1F1F22] border-none placeholder:text-light-4 focus-visible:ring-1 focus-visible:ring-offset-1 ring-offset-light-3 !important"
+                                id="password"
+                                name="password"
+                                value={password}
+                                placeholder="Enter your password"
+                                onChange={onChange}
+                            />
+                        </div>
+                        
+                        <div>
+                            <button type="submit" className="font-serif text-[18px] w-full h-12 bg-blue-600 rounded-md">
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+                </section>
+
+                <section className="my-1">
+                    <p className="font-serif text-[14px] text-white">Don't have account? <Link to='/sign-up' className="text-primary-500 text-blue-500 text-small-semibold ml-1">Sign up</Link></p>
+                </section>
+            </div>
+        </>
+    )
 }
 
 export default SigninForm
